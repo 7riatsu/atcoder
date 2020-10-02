b3a97c4a29c100e5afcb910a72f2e7c6e0c1be4c
6f8l <atsuki02jojo@gmail.com>
2020-10-02 20:34:58 +0900

    Update b

--- abc079/b.py
+++ abc079/b.py
@@ -1,9 +1,8 @@
-a = int(input())
-b = int(input())
+n = int(input())
+l = []
+l.append(2)
+l.append(1)
 
-if a > b:
-    print("GREATER")
-elif a < b:
-    print("LESS")
-else:
-    print("EQUAL")
+for i in range(2, n + 1):
+    l.append(l[i - 1] + l[i - 2])
+print(l[-1])
