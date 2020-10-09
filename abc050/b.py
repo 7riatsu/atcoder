c953a3a754bfd560b86ead678075e8dfe2472110
6f8l <atsuki02jojo@gmail.com>
2020-10-09 13:43:42 +0900

    Solve b

---
+++ abc050/b.py
@@ -0,0 +1,7 @@
+n = int(input())
+arr = list(map(int, input().split()))
+m = int(input())
+
+for _ in range(m):
+    p, x = map(int, input().split())
+    print(sum(arr[0:p - 1]) + sum(arr[p:]) + x)
