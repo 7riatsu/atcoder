28078d552ccd8d43a1c1402289f1b92eba5cad16
6f8l <atsuki02jojo@gmail.com>
2020-10-23 17:11:27 +0900

    Solve b

---
+++ abc097/b.py
@@ -0,0 +1,12 @@
+x = int(input())
+
+ans = 1
+
+for i in range(1, x + 1):
+    for j in range(2, x + 1):
+        if i ** j <= x:
+            ans = max(ans, i ** j)
+        else:
+            break
+
+print(ans)
