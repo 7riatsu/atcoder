a49d199192f8426e01eef71e654b029dcc991f1a
6f8l <atsuki02jojo@gmail.com>
2020-11-10 21:30:08 +0900

    Solve c

--- abc145/c.py
+++ abc145/c.py
@@ -1,12 +1,17 @@
-def average_length(s, t, a, b, u):
-    if u == s:
-        return a - 1, b
-    elif u == t:
-        return a, b - 1
-    return
+import itertools
+import math
 
-if __name__ == "__main__":
-    s, t = map(str, input().split())
-    a, b = map(int, input().split())
-    u = input()
-    print(average_length(s, t, a, b, u))
+def distance(x1, y1, x2, y2):
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1/2)
+
+n = int(input())
+arr = []
+
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+dist_sum = 0
+
+for team in itertools.permutations(list(range(n)), n):
+    for i in range(len(team) - 1):
+        dist_sum += distance(arr[team[i]][0], arr[team[i]][1], arr[team[i + 1]][0], arr[team[i + 1]][1])
+print(dist_sum / math.factorial(n))
