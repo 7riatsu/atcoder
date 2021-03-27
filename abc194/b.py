529c19098fc6656bc017d2e02fdea374963f8036
6f8l <atsuki02jojo@gmail.com>
2021-03-27 18:46:22 +0900

    Solve b

---
+++ abc194/b.py
@@ -0,0 +1,14 @@
+n = int(input())
+a = [0 for i in range(n)]
+b = [0 for i in range(n)]
+
+for i in range(n):
+    a[i], b[i] = map(int, input().split())
+
+ans = 10e5 ** 2
+
+for i in range(n):
+    for j in range(n):
+        ans = min(ans, a[i] + b[j] if i == j else max(a[i], b[j]))
+
+print(ans)
